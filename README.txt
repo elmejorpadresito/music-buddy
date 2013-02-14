87cc29562bec14707ebaaa4c38de37e6fa1e9fb3
Juan Fuentes <Juan Fuentes@localhost>
2013-02-14 01:33:10 -0600

    Version 1.0.1 - Rebuild application, ready to run, added temporary splash screen, compatible with more Android Versions.

---
+++ README.txt
@@ -0,0 +1,35 @@
+Authors:
+Juan Antonio Fuentes (Owner - Main Developer)
+Lauren VanVleet (Owner - Main Developer)
+Corby Schmitz (Tester - Have fun hacking)
+Amy Klouse (UI Tester - Non-commiting role)
+Anthony Borrow, S.J. (Committer - Moodle Integration Specialist)
+Gabriel Fuentes (UI Tester - Possibly committing role in the future)
+
+
+
+Main Mercurial commands:
+
+first hg add
+then hg commit (-m)
+then hg push
+
+hg pull
+hg update
+
+
+
+Eclipse Instructions:
+This is a file that was added through Eclipse. I will push the changed to test successful addition.
+
+1. Download Mercurial Eclipse
+2. On Eclipse go to File and Select Import
+3. Select to clone a Mercurial repository and then fill out the information.
+4. The project will be imported to the workstation.
+5. UPDATE contstantly to avoid running into conflics.
+6. Commit the changes.
+7. Push the changes.
+
+Let me know if you have any questions, I'm still working on getting used to this.
+
+-JAF 
---
+++ src/edu/luc/mb/Splash.java
@@ -0,0 +1,61 @@
+package edu.luc.mb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class Splash extends Activity {
+
+	/**
+	 * The thread to process splash screen events
+	 */
+	private Thread mSplashThread;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Splash screen view
+		setContentView(R.layout.splash);
+
+		final Splash sPlashScreen = this;
+
+		// The thread to wait for splash screen events
+		mSplashThread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					synchronized (this) {
+						// Wait given period of time or exit on touch
+						wait(2000);
+					}
+				} catch (InterruptedException ex) {
+				}
+
+				finish();
+
+				// Run next activity
+				Intent intent = new Intent();
+				intent.setClass(sPlashScreen, MainActivity.class);
+				startActivity(intent);
+			}
+		};
+
+		mSplashThread.start();
+	}
+
+	/**
+	 * Processes splash screen touch events
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent evt) {
+		if (evt.getAction() == MotionEvent.ACTION_DOWN) {
+			synchronized (mSplashThread) {
+				mSplashThread.notifyAll();
+			}
+		}
+		return true;
+	}
+}
