fe15977024eecc3f0e0fd26f99c283590fe4b333
lvanvlee <lvanvlee@localhost>
2013-03-20 19:03:59 -0500

    Version 1.1.1 - HorizontalListView can now show multiple image files

---
+++ src/edu/luc/mb/HorizontalListViewDemo.java
@@ -0,0 +1,78 @@
+package edu.luc.mb;
+
+
+import edu.luc.mb.HorizontalListView;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.*;
+import android.view.View;
+
+
+public class HorizontalListViewDemo extends Activity {
+
+	private static String[] values;
+	private static int[] images;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		Bundle extras = getIntent().getExtras();
+		values = extras.getStringArray("values");
+		images = extras.getIntArray("images");
+
+		setContentView(R.layout.horizontal_list_view);
+
+		HorizontalListView listview = (HorizontalListView) findViewById(R.id.listview);
+		listview.setAdapter(mAdapter);
+	}
+
+	private BaseAdapter mAdapter = new BaseAdapter() {
+
+		private OnClickListener mOnButtonClicked = new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(HorizontalListViewDemo.this);
+					builder.setMessage("hello from " + v);
+					builder.setPositiveButton("Cool", null);
+					builder.show();
+				}
+		};
+
+		@Override
+		public int getCount() {
+			return values.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View rowView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view_item, null);
+			TextView title = (TextView) rowView.findViewById(R.id.title);
+			ImageView imageView = (ImageView) rowView.findViewById(R.id.image);
+	
+			imageView.setImageResource(images[position]);
+	
+			title.setText(values[position]);
+	
+			return rowView;
+		} 
+
+	};
+}
--- src/edu/luc/mb/Learn.java
+++ src/edu/luc/mb/Learn.java
@@ -8,6 +8,18 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class Learn extends Activity {
+	
+	private static String[] NOTES = new String[]{"Treble Clef Middle C", "Treble Clef D", "Treble Clef E", "Treble Clef F", "Treble Clef G", "Treble Clef A",
+												 "Treble Clef B", "Treble Clef C", "Treble Clef D", "Treble Clef E", "Treble Clef F", "Treble Clef G",
+												 "Treble Clef A", "Treble Clef B", "Treble Clef C"};
+	
+	private static int[] IMAGES = new int[] {R.drawable.treb_middle_c, R.drawable.treb_d, R.drawable.treb_e, R.drawable.treb_f, R.drawable.treb_g, R.drawable.treb_a,
+											 R.drawable.treb_b, R.drawable.treb_c, R.drawable.treb_d1, R.drawable.treb_e1, R.drawable.treb_f1, R.drawable.treb_g1,
+											 R.drawable.treb_a1, R.drawable.treb_b1, R.drawable.treb_c1};
+	
+	private static String[] BASS_NOTES = new String[]{"Bass Clef Middle C"};
+	private static int[] BASS_IMGS = new int[]{R.drawable.bass_middle_c};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,16 +39,32 @@ public class Learn extends Activity {
 			}
 		});
 		
-		Button b = (Button) findViewById(R.id.list_button);
+		Button b = (Button) findViewById(R.id.treb_button);
 		b.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				
-				Intent b = new Intent(Learn.this, HorizontalListViewDemo.class);  
+				Intent b = new Intent(Learn.this, HorizontalListViewDemo.class); 
+				b.putExtra("values", NOTES);
+				b.putExtra("images", IMAGES);
 				startActivity(b);
 				
 				Toast.makeText(getBaseContext(), "Get Ready to Horizontal List",
 						Toast.LENGTH_SHORT).show();
 			}
 		});
+		
+/*		Button c = (Button) findViewById(R.id.bass_button);
+		c.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				Intent c = new Intent(Learn.this, HorizontalListViewDemo.class); 
+				c.putExtra("values", BASS_NOTES);
+				c.putExtra("images", BASS_IMGS);
+				startActivity(c);
+				
+				Toast.makeText(getBaseContext(), "Get Ready to Horizontal List",
+						Toast.LENGTH_SHORT).show();
+			}
+		}); */
 	}
 }
