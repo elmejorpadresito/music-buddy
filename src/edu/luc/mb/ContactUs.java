0b30e5019a19c8cc4d17eef40ea226e52bfd1e0e
Juan Fuentes <Juan Fuentes@localhost>
2013-04-25 16:33:15 -0500

    Version 1.2.4 - Updated the Contact Us with a little thank you for all.

--- src/edu/luc/mb/ContactUs.java
+++ src/edu/luc/mb/ContactUs.java
@@ -29,6 +29,22 @@ public class ContactUs extends ListActivity {
 		super.onListItemClick(l, v, position, id);
 		switch (position) {
 		case 0:
+			AlertDialog.Builder alert = new AlertDialog.Builder(context);
+	        alert.setTitle("About Music Buddy"); //Set Alert dialog title here
+	        alert.setMessage("This application is part of an Open Source Project for Loyola University Chicago COMP 412. \nThe project is run under the GNU GPL v3 License. Comments, questions, and suggestions may be submitted to our email address: mscbdd@gmail.com"); //Message here
+	        AlertDialog alertDialog = alert.create();
+	        alertDialog.show();
+			break;
+
+		case 1:
+			AlertDialog.Builder acknoledgements = new AlertDialog.Builder(context);
+	        acknoledgements.setTitle("Acknoledgements"); //Set Alert dialog title here
+	        acknoledgements.setMessage("The Developers would like acknoledgedge the following individuals for their outstanding support and collaboration in the development of this application: \n\n-Corby Schmitz \n-Jason Ong \n-Juan Antonio Fuentes \n-Lauren Van Vleet \n-Melissa Filipiak \n-Rafal Zukowski \n\nThank You!"); //Message here
+	        AlertDialog alertDialog1 = acknoledgements.create();
+	        alertDialog1.show();
+			break;
+		
+		case 2:
 			// Start activity that you want to when 1st item of your list is
 			// selected
 			Intent DialIntent = new Intent(Intent.ACTION_DIAL,
@@ -41,7 +57,7 @@ public class ContactUs extends ListActivity {
 
 			break;
 
-		case 1:
+		case 3:
 			Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
 			emailIntent.setType("message/rfc822");
 			startActivity(emailIntent);
@@ -67,18 +83,9 @@ public class ContactUs extends ListActivity {
 
 			break;
 			
-		case 2:
-			AlertDialog.Builder alert = new AlertDialog.Builder(context);
-	        alert.setTitle("About Music Buddy"); //Set Alert dialog title here
-	        alert.setMessage("This application is part of an Open Source Project for Loyola University Chicago COMP 412. \nThe project is run under the GNU GPL v3 License. Comments, questions, and suggestions may be submitted to our email address: mscbdd@gmail.com"); //Message here
-	        AlertDialog alertDialog = alert.create();
-	        alertDialog.show();
-	        
-			break;
-
 		}
 
 	}
 
-	static final String[] NEWS = new String[] { "Call Developers - Beta", "Email Us", "About" };
+	static final String[] NEWS = new String[] { "About", "Acknoledgements", "Call Developers [Beta]", "Email Us",  };
 }
